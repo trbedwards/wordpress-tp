9763a8930920d9284d8eb780403c52ebaa9cfc9d
Omar <omf93@hotmail.com>
2014-03-20 11:28:41 +0000

    Edited twentyfourteen theme so pages display tags and categories

--- wp-content/themes/twentyfourteen/page.php
+++ wp-content/themes/twentyfourteen/page.php
@@ -29,7 +29,7 @@ get_header(); ?>
 				while ( have_posts() ) : the_post();
 
 					// Include the page content template.
-					get_template_part( 'content', 'page' );
+					get_template_part( 'content', 'post' );
 
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) {
